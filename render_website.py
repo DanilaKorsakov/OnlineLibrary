3527bffb5cd1c41d7aae6046da77f336b19f8afe
Scarlxrd <kvashnindan@gmail.com>
2023-09-01 13:56:10 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,12 +1,13 @@
 import json
 
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server, shell
 from more_itertools import chunked
+from pathlib import Path
 
 
-def on_reload():
+def on_reload(folder):
 
     with open("books.json", "r", encoding='utf-8') as my_file:
         books_json = my_file.read()
@@ -15,31 +16,40 @@ def on_reload():
 
     books = json.loads(books_json)
 
-    books_row = list(chunked(books,2))
-
-
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html'])
     )
+
     template = env.get_template('template.html')
 
-    rendered_page = template.render(
-        books_row=books_row
-    )
+    books_page =  list(chunked(books,10))
 
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    for number, page in enumerate(books_page, 1):
+
+        books_row = list(chunked(page,2))
+
+        rendered_page = template.render(
+            books_row=books_row,
+        )
+
+        with open(f'{folder}/index{number}.html', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 def main():
-    on_reload()
 
-    # server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-    # server.serve_forever()
+    folder = 'pages/'
+
+    Path(folder).mkdir(parents=True, exist_ok=True)
+
+    on_reload(folder)
+
 
     server = Server()
     server.watch('template.html', on_reload)
     server.serve(root='.')
+
+
 
 
 if __name__ == '__main__':
