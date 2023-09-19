c26a728a9142d8f90226883f34f24d25faa35234
Scarlxrd <kvashnindan@gmail.com>
2023-09-19 16:13:38 +0300

    pep 8 styling

--- render_website.py
+++ render_website.py
@@ -7,58 +7,52 @@ from livereload import Server
 from more_itertools import chunked
 
 
-
 def on_reload():
 
     parser = argparse.ArgumentParser(
-        description='Программа предоставляет доступ к сайту с книгами'
+        description = 'Программа предоставляет доступ к сайту с книгами'
     )
     parser.add_argument("--json_path", type=str, help="путь к JSON файлу", default='books.json')
     args = parser.parse_args()
 
-    with open(args.json_path, "r", encoding='utf-8') as my_file:
+    with open(args.json_path, "r", encoding = 'utf-8') as my_file:
         books_json = my_file.read()
 
     books = json.loads(books_json)
 
     env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html'])
+        loader = FileSystemLoader('.'),
+        autoescape = select_autoescape(['html'])
     )
-
     template = env.get_template('template.html')
 
-    per_page_books=  list(chunked(books,10))
-
+    per_page_books = list(chunked(books,10))
     pages_count = len(per_page_books)
 
     for number, page in enumerate(per_page_books, 1):
-
         row_of_books = list(chunked(page,2))
 
         rendered_page = template.render(
-            row_of_books=row_of_books,
+            row_of_books = row_of_books,
             page_number = number,
-            pages_count=pages_count
+            pages_count = pages_count
         )
 
-        with open(f'pages/index{number}.html', 'w', encoding="utf8") as file:
+        with open(f'pages/index{number}.html', 'w', encoding = "utf8") as file:
             file.write(rendered_page)
+
 
 def main():
 
     folder = 'pages/'
 
-    Path(folder).mkdir(parents=True, exist_ok=True)
+    Path(folder).mkdir(parents = True, exist_ok = True)
 
     on_reload()
 
-
     server = Server()
-    server.watch('template.html',on_reload)
-    server.serve(root='.', default_filename='pages/index1.html')
-
-
+    server.watch('template.html', on_reload)
+    server.serve(root='.',  default_filename='pages/index1.html')
 
 
 if __name__ == '__main__':
